6aff609fb4ca064ea9a88def9b09100bfdbe158e
Adam Gray <adam@pateo.co>
2017-07-24 08:10:50 +0100

    Complete todos routes

--- server/api/v1/todos/index.js
+++ server/api/v1/todos/index.js
@@ -2,22 +2,55 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../../../db/models').Todo
 
-router.get('/todos', (req, res) => {
-  Todo.findAll().then((todos) => {
-    res.json(todos)
+router.route('/todos')
+
+  .get((req, res) => {
+    Todo.findAll().then((todos) => {
+      res.json(todos)
+    })
   })
-})
 
-router.post('/todos', (req, res) => {
-  res.send('POST /todos')
-})
+  .post((req, res) => {
+    Todo.create({
+      action: req.body.action
+    }).then((todo) => {
+      res.json(todo)
+    })
+  })
 
-router.put('/todos', (req, res) => {
-  res.send('PUT /todos')
-})
+router.route('/todos/todo_id')
 
-router.delete('/todos', (req, res) => {
-  res.send('DELETE /todos')
-})
+  .get((req, res) => {
+    Todo.findAll({
+      where: {
+        id: req.params.todo_id
+      }
+    }).then((todo) => {
+      res.json(todo)
+    })
+  })
+
+  .put((req, res) => {
+    Todo.update({
+      action: req.body.action
+    }, {
+      where: {
+        id: req.params.todo_id
+      }
+    }).then((account) => {
+      res.json({ message: 'Todo updated.' })
+    })
+  })
+
+  .delete((req, res) => {
+    Todo.destroy({
+      where: {
+        id: req.params.todo_id
+      }
+    }).then(() => {
+      res.json({ message: 'Todo destroyed.' })
+    })
+  })
+
 
 module.exports = router
