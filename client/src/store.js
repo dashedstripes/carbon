d2f48d667e84f18a9213710e497bcf2c410977b5
Adam Gray <adam@pateo.co>
2017-07-24 14:33:29 +0100

    Enable redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
-let store = createStore(rootReducer)
+let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 store.subscribe(() => {
   console.log(store.getState())
